f5e5576c893584b362982992429cfef6719220f9
Angie Walker <angiewalker@Angies-MacBook-Pro.local>
2022-06-10 15:20:35 -0400

    Backup - done for the day Friday Jun 10

--- downloadAllData.php
+++ downloadAllData.php
@@ -19,12 +19,11 @@ if (mysqli_num_rows($result) > 0) {
   $filename = "AllData_" . date("Y-m-d") . ".csv";
 
   $fp = fopen('php://memory', 'w');
-
-  $fields = array("Specimen", "Species", "Initials", "Experiment", "Site", "Visit", "Celltype", "Cellnumber", "Sampledate", "Room", "Freezer", "Cane", "Box", "Row", "Column", "WrittenonTube/Notes","Removed from Box for ELISA fridge?", "IgG/Spike", "IgG/SpikeValue", "Counter", "IgGTitration");
+  $fields = array("Specimen", "Species", "Initials", "Experiment", "Site", "Visit", "Celltype", "Cellnumber", "Sampledate", "Room", "Freezer", "Cane", "Box", "Row", "Column", "WrittenonTube_Notes","RemovedfromBoxforELISAfridge?", "IgG_Spike", "IgG_SpikeValue", "Counter", "IgGTitration");
   fputcsv($fp, $fields, $delimiter);
 
   while($row = mysqli_fetch_assoc($result)) {
-    $lineData = array($row['Specimen'], $row['Species'], $row['Initials'], $row['Experiment'], $row['Site'], $row['Visit'], $row['Celltype'], $row['Cellnumber'], $row['Sampledate'], $row['Room'], $row['Freezer'], $row['Cane'], $row['Box'], $row['Row'], $row['Column'], $row['Written on Tube/Notes'], $row['Removed from Box for ELISA fridge?'], $row['IgG/Spike'], $row['IgG/Spike Value'], $row['Counter'], $row['IgGTitration']); 
+    $lineData = array($row['Specimen'], $row['Species'], $row['Initials'], $row['Experiment'], $row['Site'], $row['Visit'], $row['Celltype'], $row['Cellnumber'], $row['Sampledate'], $row['Room'], $row['Freezer'], $row['Cane'], $row['Box'], $row['Row'], $row['Column'], $row['WrittenonTube_Notes'], $row['RemovedfromBoxforELISAfridge'], $row['IgG_Spike'], $row['IgG_SpikeValue'], $row['Counter'], $row['IgGTitration']); 
     fputcsv($fp, $lineData, $delimiter); 
   }
 
--- inputnewvalues.php
+++ inputnewvalues.php
@@ -31,16 +31,12 @@ $igvalue = $_POST["IgG/SpikeValue"];
 $counter = $_POST["Counter"];
 $igtit = $_POST["IgG/Titration"];
 
-
-$selectionline = "WHERE ";
+$insertline1 = "INSERT INTO testing.top1000_temp ("
+$insertline2 = "VALUES ("
 
 if ($specimen != "") {
-   if ($selectionline == "WHERE ") {
-      $selectionline = $selectionline . 'Specimen="'	. $specimen . '"';
-   }
-   else {
-      $selectionline = $selectionline . ' AND Specimen="' . $specimen . '"';
-   }
+   $insertline1 = $insertline1 . "Specimen,";
+   $insertline2 = $insertline2 . ""
 }
 
 if ($species != "Select species") {
@@ -214,37 +210,10 @@ if ($igtit != "IgGTitration") {
    }
 }
 
-$selectionline = $selectionline . ";";
 
-// save to file
-
-$sql = "SELECT * FROM testing.top1000_temp " . $selectionline;
+$sql = "SELECT * FROM testing.top1000_temp ";
 $result = $conn->query($sql);
 
-if (mysqli_num_rows($result) > 0) {
-  $delimiter = ",";
-  $filename = "testing_" . date("Y-m-d") . ".csv";
+echo "Sample successfully added!";
 
-  $fp = fopen('php://memory', 'w');
-
-  $fields = array("Specimen", "Species", "Initials", "Experiment", "Site", "Visit", "Celltype", "Cellnumber", "Sampledate", "Room", "Freezer", "Cane", "Box", "Row", "Column", "WrittenonTube_Notes","RemovedfromBoxforELISAfridge?", "IgG_Spike", "IgG_SpikeValue", "Counter", "IgGTitration");
-  fputcsv($fp, $fields, $delimiter);
-
-  while($row = mysqli_fetch_assoc($result)) {
-    $lineData = array($row['Specimen'], $row['Species'], $row['Initials'], $row['Experiment'], $row['Site'], $row['Visit'], $row['Celltype'], $row['Cellnumber'], $row['Sampledate'], $row['Room'], $row['Freezer'], $row['Cane'], $row['Box'], $row['Row'], $row['Column'], $row['WrittenonTube_Notes'], $row['RemovedfromBoxforELISAfridge'], $row['IgG_Spike'], $row['IgG_SpikeValue'], $row['Counter'], $row['IgGTitration']); 
-    fputcsv($fp, $lineData, $delimiter); 
-  }
-
-  fseek($fp, 0);
-
-  header('Content-Type: text/csv'); 
-  header('Content-Disposition: attachment; filename="' . $filename . '";'); 
-     
-  fpassthru($fp);
-}
-else {
-
-  echo "No results.";
-}
-
-?>
+?>B
